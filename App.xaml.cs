7f57c36a4c187b8991a6b9d708af529754973859
Utkarsh2105 <dutkarsh05@gmail.com>
2024-09-24 07:29:08 +0530

    Initials

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace TriggersApplication
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+	}
+
+}
